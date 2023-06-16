a390760250a95b01866871640b3009fdc41f48d5
arjybltrn <arjaybeltran16@gmail.com>
2023-06-16 15:29:07 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, 
+TouchableNativeFeedback, TouchableHighlight, TouchableWithoutFeedback, TouchableOpacity,
+ Alert, Button, Platform, Dimensions } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
+  // console.log(Dimensions.get("screen")) // get dimensions of device
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <View style={{
+            backgroundColor: "white",
+            flex: 1,
+            flexDirection: "row", // horizontal
+            justifyContent: "center", //main
+            alignItems: "center", //secondary 
+          }}>
+
+          
+            <View style={{
+                backgroundColor: "dodgerblue",
+                width: 100,
+                height: 100,
+              }}>
+
+            </View>
+
+            <View style ={{
+                backgroundColor: "gold",
+                width: 100,
+                height: 100,
+              }}>
+
+            </View>
+
+            <View style ={{
+                backgroundColor: "tomato",
+                width: 100,
+                height: 100,
+              }}>
+
+            </View>
+        </View>
+  
+
   );
 }
+
+const containerStyle = { backgroundColor: "wheat" }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
