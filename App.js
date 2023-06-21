73077919fc2ea49bf9e5a93864a6db8d4b5f4899
arjybltrn <arjaybeltran16@gmail.com>
2023-06-21 15:30:45 -0400

    add new file MessagesScreen.js

--- App.js
+++ App.js
@@ -11,12 +11,13 @@ import colors from './app/config/colors'
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card'
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
 
 
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
 
       );
   }
