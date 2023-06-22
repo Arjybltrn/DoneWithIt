640f6635651ba4a3a1af71fbf183646266779076
arjybltrn <arjaybeltran16@gmail.com>
2023-06-22 15:32:24 -0400

    adds AppTextInput

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 
 import ListingsScreen from './app/screens/ListingsScreen';
+import { TextInput, Text } from 'react-native'
+import Screen from './app/components/Screen';
+import React, { useState } from 'react'
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
 
+  const [firstName, setFirstName] = useState("")
 
   return (
-    <ListingsScreen />
+    <Screen>
+      <AppTextInput placeholder="username" icon="email" />
+    </Screen>
 
       );
   }
