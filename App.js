5fbcf81d121157d9b7b7bc891d5a88cf81b6d422
arjybltrn <arjaybeltran16@gmail.com>
2023-06-22 16:10:36 -0400

    adds AppPicker using TouchableOpacity

--- App.js
+++ App.js
@@ -6,18 +6,27 @@ import React, { useState } from 'react'
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
+const categories = [
+   {
+    label: "Furniture", value: 1 
+   },
+   {
+    label: "Clothing", value: 2 
+   },
+   {
+    label: "Cameras", value: 3 
+   },
+]
+
 export default function App() {
 
   const [isNew, setIsNew] = useState(false)
 
   return (
     <Screen>
-      <AppPicker icon="apps" placeholder="Category" />
+      <AppPicker items={categories} icon="apps" placeholder="Category" />
       <AppTextInput icon="email" placeholder="email"/>
 
-      <Switch 
-        value={isNew}
-        onValueChange={newValue => setIsNew(newValue)}/>
     </Screen>
 
       );
