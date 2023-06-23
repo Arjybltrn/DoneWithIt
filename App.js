93ff52ef05b8cf80c80123fba0fd645d21b810c4
arjybltrn <arjaybeltran16@gmail.com>
2023-06-22 21:41:20 -0400

    input field dynamic width

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import AppText from "./app/components/AppText";
+import AccountScreen from "./app/screens/AccountScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingEditScreen />
 }
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -12,13 +12,13 @@ const initialMessages = [
   {
     id: 1,
     title: "T1",
-    description: "D1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     image: require("../assets/mosh.jpg"),
   },
   {
     id: 2,
     title: "T2",
-    description: "D2",
+    description: "Hey, is this still available?",
     image: require("../assets/mosh.jpg"),
   },
 ];
